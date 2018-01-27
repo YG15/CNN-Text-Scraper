9cb7df7c30d7ecf4c36ed67b47cff5d8fee75396
YG15 <YG15@users.noreply.github.com>
2018-01-28 01:36:04 +0200

    Add files via upload

--- CNN_Text_scraper.py
+++ CNN_Text_scraper.py
@@ -1,17 +1,20 @@
 ##########################################
 # Name:  CNN Text Scraper
 # Target : Accessing and processing data from the CNN Transcripts site for a given range of dates
-# Parameters : days, months and years of start day and end day, "automated" if False,
-#              the script run as an example without taking dates parameters
-# Returning: A dictionary containing all the processed transcript from the CNN between the date range
+# Parameters : Start and end dates for the data collection.
+#              The script can without receiving dates, and ask for if this happens.
+#              option: set working directory
+# Returning: A collection of .txt files, a file for each transcript page.
 # Author: Yonathan Guttel <yesguttel@gmail.com>
-# Date: 26.01.2018
-# Version: 1
+# Date: 28.01.2018
+# Version: 2
 ###########################################
 
 # Packages
 from bs4 import BeautifulSoup
 import re
+import os
+import math
 import requests
 import numpy as np
 from datetime import date, timedelta
@@ -20,32 +23,27 @@ from tqdm import tqdm
 
 
 
-# The function recieve a URL (as string) and return all the children pages URL which includes transcript
-def getURL(url):
-    # correct url in case "http" wasn't entered as part of it
+# The function recieve a URL (as string) and yield all the children pages URL which includes transcript
+def get_urls(url):
+    # Correct url in case "http" wasn't entered as part of it
     if url.startswith("http"):
         pass
     else:
         url = ("http://" +url)
     resp = urllib.request.urlopen(url)
     mega_soup = BeautifulSoup(resp, "html5lib")
-    urls = []
-    #collected all suitable url in the webpage in the list "urls"
     for link in mega_soup.find_all('a', href=True):
-        # Transcripts web page starts with:
-        # "http://transcripts.cnn.com/TRANSCRIPTS/" and have an additional part afterwood
         if (link['href'].startswith("/TRANSCRIPTS/") and len(link['href']) > 13) == True:
-            urls.append("http://transcripts.cnn.com" + link['href'])
-    return (urls)
+            yield("http://transcripts.cnn.com" + link['href'])
 
-# the function collct the text from a given url and process it to remove
-# names of speaker, remark on the transcript and unnessecary charchters
+
+# The function collect the text from a given url and process it to remove
+# names of speaker, remark on the transcript and unnecessary characters
 def text_scraper(url):
     if url.startswith("http"):
-        r= requests.get(url)
+        data = urllib.request.urlopen(url)
     else:
-        r  = requests.get("http://" +url)
-    data = urllib.request.urlopen(url)
+        data  = urllib.request.urlopen("http://" +url)
     soup = BeautifulSoup(data, "html5lib")
     #raw_text=soup.get_text(strip=True) # if we want full text, less useful
     #raw text as list of paragraphs:
@@ -71,58 +69,58 @@ def text_scraper(url):
         cut_text[i] = re.sub(r'([\[\]\\\/\)\(])', '', cut_text[i])
         final_text_list.append(cut_text[i])
     final_text= ' '.join(final_text_list)
-    #returning clean text
+    # Returning clean text
     return (final_text)
 
-# The function recieves dates and use the function above to output a dictionary of processed texts
-def all_dates_range_texts(automated):
-    print("This is a CNN transcripts web text scraper, please enter the date from which you want to draw text from...")
-    start_date =date(1900,1,1)
-    while start_date<date(1999,9,11):
-        print ("Start day has to be after September 11th 1999")
-        #until mid 2000's there are missing days and problematic transcripts
-        # which cause bug in the script- shoul be later fixed
-        if automated == True:
-            year_start = input("Enter the year to start from from [4 digits]: ")
-            month_start = input("Enter the month to start from from [2 digits]: ")
-            day_start = input("Enter the day to start from from [2 digits]: ")
-            year_end = input("Enter the year to stop at [4 digits]: ")
-            month_end = input("Enter the month to stop at [2 digits]: ")
-            day_end = input("Enter the day to stop at [2 digits]: ")
-        else:
-            print("example case presented dates between 24.01.2018 to 24.01.2018")
-            year_start = 2018
-            year_end = 2018
-            month_start = 1
-            month_end = 1
-            day_start = 24
-            day_end = 24
+# The function recieves two dates and yeilds the days between them
+def get_days_in_range(start_date_s,end_date_s):
+    # Checking if both dates suits the desireable formats
+    cond1 = re.match(r'^(\d{4}\.\d{2}\.\d{2})', str(start_date_s))
+    cond2 = re.match(r'^(\d{4}\.\d{2}\.\d{2})', str(end_date_s))
+    # If not, it request the dates
+    if not (cond1 or cond2):
+        print("You haven't inputted date range ")
+        start_date_s = input("Please enter the start date in format YYYY.MM.DD: ")
+        end_date_s = input("Please enter the end date in format YYYY.MM.DD: ")
+    # After dates are set, find and yield the days between them
+    start_date = date(int(start_date_s[0:4]), int(start_date_s[5:7]), int(start_date_s[9:11]))
+    end_date = date(int(end_date_s[0:4]), int(end_date_s[5:7]), int(end_date_s[9:11]))
+    delta = end_date - start_date  # time delta
+    for i in range(delta.days + 1):
+        yield((start_date + timedelta(days=i)).strftime("%Y.%m.%d"))
 
-        start_date = date(int(year_start), int(month_start), int(day_start))
-        end_date = date(int(year_end), int(month_end), int(day_end))
-        delta = end_date - start_date  # timedelta
-        days_list = []
-        for i in range(delta.days + 1):
-            days_list.append((start_date + timedelta(days=i)).strftime("%Y.%m.%d"))
 
-    all_urls = []
-    for day in days_list:
+# The function recieves dates and use the functions above to output a the texts of al transcripts
+# in the date ranges, the file are kept as seperate txt file in the directory
+def get_texts_in_range(start_date_s=math.nan,end_date_s=math.nan):
+    days_list=get_days_in_range(start_date_s,end_date_s)
+    print("Please wait while processing request....")
+    for day in tqdm(days_list):
         day_url = ("http://transcripts.cnn.com/TRANSCRIPTS//" + day + ".html")
-        day_child_urls = getURL(day_url)
-        all_urls.extend(day_child_urls)
+        urls = get_urls(day_url)
+        for url in tqdm(urls):
+            raw_title = re.search(r'TRANSCRIPTS\/(.*?)\.html',url).group(1)
+            final_title = re.sub(r'([\/\.])', '_', raw_title)
+            with open((str(final_title)+".txt"), "w") as f:
+                f.write(text_scraper(url))
 
-    text_dic={}
-    print ("Please wait while processing request....")
-    for url in tqdm(all_urls):
-        text_dic [str(url)] = text_scraper(url)
-    return (text_dic)
 
 if __name__ == "__main__":
 
-    automated = True
+    """
+    #The following part is only for people who uses the script without setting the wd before, It is not really needed
+        # See which directory is the current directory to keep files
+        print("The files will be saved in the folder: ", os.getcwd())
+        answer =input("If you wish to continue please type Y, if you wish to change directory please type N ")
+        # If needed, set new directory
+        if answer !="Y":
+            new_wd=input("please enter the path to the folder where the file should be saved: ")
+            os.chdir(new_wd)
+    """
+    # Run the script
+    # With  given dates
+    #get_texts_in_range("2018.01.01","2018.01.01")
 
-    #run the script
-    text_dic = all_dates_range_texts(automated)
-    #checking if it worked on a sample
-    print(next (iter (text_dic.values())))
+    # Without given dates
+    get_texts_in_range()
 
